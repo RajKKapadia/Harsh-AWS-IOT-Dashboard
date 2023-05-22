3d2133fb64d80781484ba8d0ada4868b25668d03
RajKKapadia <raajforyou@gmail.com>
2023-05-22 13:00:36 +0530

    added aws activation

--- server/controllers/tagController.js
+++ server/controllers/tagController.js
@@ -1,37 +1,53 @@
+const axios = require('axios');
+
 const TagModel = require('../models/tagModel');
 
 const createOrUpdateTag = async (req, res) => {
-    let machineId = req.params.id;
-    let tagBody = req.body;
 
-    let tag = await TagModel.findOne({ tag: tagBody.tag, machineId: machineId });
-
-    let result = {};
-
-    if (!tag) {
-        result = await TagModel.create(
-            {
-                machineId: machineId,
-                tag: tagBody.tag,
-                data: [
-                    tagBody.v
-                ]
-            }
-        );
+    if (req.body.hasOwnProperty('enableUrl')) {
+        let options = {
+            method: 'GET',
+            url: req.body.enableUrl
+        };
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+        res.sendStatus(200);
     } else {
-        result = await TagModel.findOneAndUpdate(
-            {
-                tag: tagBody.tag, machineId: machineId
-            },
-            {
-                $push: { data: tagBody.v }
-            }
-        );
+        let machineId = req.params.id;
+        let tagBody = req.body;
+
+        let tag = await TagModel.findOne({ tag: tagBody.tag, machineId: machineId });
+
+        let result = {};
+
+        if (!tag) {
+            result = await TagModel.create(
+                {
+                    machineId: machineId,
+                    tag: tagBody.tag,
+                    data: [
+                        tagBody.v
+                    ]
+                }
+            );
+        } else {
+            result = await TagModel.findOneAndUpdate(
+                {
+                    tag: tagBody.tag, machineId: machineId
+                },
+                {
+                    $push: { data: tagBody.v }
+                }
+            );
+        }
+        return res.status(200).json(result);
     }
-    return res.status(200).json(result);
 };
 
-const gettagsByMachineId = async (req,res) =>{
+const gettagsByMachineId = async (req, res) => {
     let machineId = req.params.id
     let tag = await TagModel.find({ machineId: machineId });
     return res.status(200).json(tag)
